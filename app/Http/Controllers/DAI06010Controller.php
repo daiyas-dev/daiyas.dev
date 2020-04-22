60c14d026b478cc4ac6eb742125f1199439edcff
hitoshi-katsumata <katsumata@n-e-w.jp>
2020-04-22 19:19:25 +0900

    06010 作成中

--- app/Http/Controllers/DAI06010Controller.php
+++ app/Http/Controllers/DAI06010Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use DB;
 use Illuminate\Support\Carbon;
+use PDO;
 
 class DAI06010Controller extends Controller
 {
@@ -15,35 +16,66 @@ class DAI06010Controller extends Controller
     public function Search($vm)
     {
         $BushoCd = $vm->BushoCd;
+        $DateStart=$vm->DateStart;
+        $DateEnd=$vm->DateEnd;
+        $CustomerCd=$vm->CustomerCd;
+
+        //処理選択
+        $sql_customer_cd="";
+        if ($vm->SearchCondition=="2" && $CustomerCd!=null) {
+            $sql_customer_cd=" AND D1.得意先ＣＤ = $CustomerCd";
+        }
+
+        //発行範囲
+        $sql_date_period="";
+        if ($vm->IssuePeriod=="2" && $DateStart!=null && $DateEnd!=null) {
+            $sql_date_period="　AND 発行日 >= '$DateStart'　AND 発行日 <= '$DateEnd'";
+        }
 
         $sql = "
                 SELECT
-                  得意先マスタ.得意先ＣＤ
-                , 得意先名
-                , 得意先名カナ
-                , 郵便番号
-                , 住所１
-                , 電話番号1
-                , 売掛現金区分
-                , 締区分
-                , 締日１
-            FROM
-                [得意先マスタ]
-                LEFT JOIN [コーステーブル]
-                ON コーステーブル.得意先ＣＤ = CASE
-                    WHEN 得意先マスタ.受注得意先ＣＤ = 0 THEN 得意先マスタ.得意先ＣＤ
-                    ELSE 得意先マスタ.受注得意先ＣＤ
-                    END
-                AND 得意先マスタ.部署ＣＤ = コーステーブル.部署ＣＤ
-            where
-                コーステーブル.コースＣＤ is null
-                AND 得意先マスタ.部署ＣＤ = $BushoCd
-            order by
-                コーステーブル.コースＣＤ
-                , 得意先マスタ.得意先ＣＤ
-            ";
-        $DataList = DB::select(DB::raw($sql));
+                      チケット管理番号
+                    , D1.得意先ＣＤ
+                    , 得意先名
+                    , FORMAT(D1.発行日,'yyyy/MM/dd')AS F発行日
+                    , FORMAT(有効期限,'yyyy/MM/dd')AS F有効期限
+                    , FORMAT(印刷日時,'yyyy/MM/dd')AS F印刷日時
+                    , チケット内数
+                    , SV内数
+                    , 単価
+                    , D1.担当者ＣＤ
+                    , M2.担当者名
+                    , 廃棄
+                    , M1.得意先名カナ
+                FROM
+                    [チケット発行] D1
+                    INNER JOIN [得意先マスタ] M1
+                        ON M1.得意先ＣＤ = D1.得意先ＣＤ
+                    INNER JOIN [担当者マスタ] M2
+                        ON M2.担当者ＣＤ = D1.担当者ＣＤ
+                WHERE
+                    M1.部署CD = $BushoCd
+                    $sql_date_period
+                    $sql_customer_cd
+                ";
 
-        return response()->json($DataList);
+        //出力順
+        $order="";
+        if ($vm->PrintOrder=="1") {
+            $order=" order by M1.得意先名カナ";
+        }else{
+            $order=" order by D1.得意先ＣＤ,D1.発行日 DESC";
+        }
+        $sql.=$order;
+
+        $dsn = 'sqlsrv:server=127.0.0.1;database=daiyas';
+        $user = 'daiyas';
+        $password = 'daiyas';
+
+        $pdo = new PDO($dsn, $user, $password);
+        $stmt = $pdo->query($sql);
+        $DataList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $DataList;
     }
 }
--- resources/js/components/Pages/DAI06010.vue
+++ resources/js/components/Pages/DAI06010.vue
@@ -10,6 +10,121 @@
                 />
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-1">
+                <label>処理選択</label>
+            </div>
+            <div class="col-md-5">
+                <VueOptions
+                    id="SearchCondition"
+                    ref="VueOptions_SearchCondition"
+                    customItemStyle="text-align: center; margin-right: 10px;"
+                    :vmodel=viewModel
+                    bind="SearchCondition"
+                    :list="[
+                        {code: '1', name: '部署全て', label: '1:部署全て'},
+                        {code: '2', name: '得意先指定', label: '2:得意先指定'},
+                    ]"
+                    :onChangedFunc=onChanged
+                />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <label>得意先</label>
+            </div>
+            <div class="col-md-4">
+                <PopupSelect
+                    id="CustomerSelect"
+                    ref="PopupSelect_Customer"
+                    :vmodel=viewModel
+                    bind="CustomerCd"
+                    buddy="CustomerNm"
+                    dataUrl="/Utilities/GetCustomerListForSelect"
+                    :params="{ BushoCd: viewModel.BushoCd, CourseCd:null, KeyWord: null }"
+                    :isPreload=true
+                    title="得意先一覧"
+                    labelCd="得意先CD"
+                    labelCdNm="得意先名"
+                    :showColumns='[
+                        { title: "部署名", dataIndx: "部署名", dataType: "string", width: 120, maxWidth: 120, minWidth: 120, colIndx: 0 },
+                    ]'
+                    :popupWidth=1000
+                    :popupHeight=600
+                    :isShowName=true
+                    :isModal=true
+                    :editable=true
+                    :reuse=true
+                    :existsCheck=true
+                    :inputWidth=100
+                    :nameWidth=250
+                    :onAfterChangedFunc=onChanged
+                    :isShowAutoComplete=true
+                    :AutoCompleteFunc=CustomerAutoCompleteFunc
+                />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <label>発行範囲</label>
+            </div>
+            <div class="col-md-2">
+                <VueOptions
+                    id="IssuePeriod"
+                    ref="VueOptions_IssuePeriod"
+                    customItemStyle="text-align: center; margin-right: 10px;"
+                    :vmodel=viewModel
+                    bind="IssuePeriod"
+                    :list="[
+                        {code: '1', name: '全期間', label: '1:全期間'},
+                        {code: '2', name: 'コース順', label: '2:範囲'},
+                    ]"
+                    :onChangedFunc=onChanged
+                />
+            </div>
+            <div class="col-md-4">
+                <DatePickerWrapper
+                    id="DateStart"
+                    ref="DatePicker_Date"
+                    format="YYYY年MM月DD日"
+                    dayViewHeaderFormat="YYYY年MM月"
+                    :vmodel=viewModel
+                    bind="DateStart"
+                    :editable=true
+                    :onChangedFunc=onChanged
+                />
+                <label>～</label>
+                <DatePickerWrapper
+                    id="DateEnd"
+                    ref="DatePicker_Date"
+                    format="YYYY年MM月DD日"
+                    dayViewHeaderFormat="YYYY年MM月"
+                    :vmodel=viewModel
+                    bind="DateEnd"
+                    :editable=true
+                    :onChangedFunc=onChanged
+                />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <label>出力順</label>
+            </div>
+            <div class="col-md-5">
+                <VueOptions
+                    id="PrintOrder"
+                    ref="VueOptions_PrintOrder"
+                    customItemStyle="text-align: center; margin-right: 10px;"
+                    :vmodel=viewModel
+                    bind="PrintOrder"
+                    :list="[
+                        {code: '1', name: '顧客カナ順', label: '1:顧客カナ順'},
+                        {code: '2', name: '発効日降順', label: '2:発効日降順'},
+                    ]"
+                    :onChangedFunc=onChanged
+                />
+            </div>
+        </div>
         <PqGridWrapper
             id="DAI06010Grid1"
             ref="DAI06010Grid1"
@@ -66,10 +181,16 @@ export default {
     },
     data() {
         return $.extend(true, {}, PageBaseMixin.data(), {
-            ScreenTitle: "随時処理 > コース外得意先問合せ",
+            ScreenTitle: "チケット > チケット印刷管理",
             noViewModel: true,
             viewModel: {
                 BushoCd: null,
+                SearchCondition:"2",
+                CustomerCd: null,
+                IssuePeriod:"2",
+                DateStart:null,
+                DateEnd:null,
+                PrintOrder:"2",
             },
             DAI06010Grid1: null,
             grid1Options: {
@@ -82,7 +203,7 @@ export default {
                 autoRow: false,
                 rowHtHead: 40,
                 rowHt: 30,
-                freezeCols: 2,
+                freezeCols: 3,
                 editable: false,
                 columnTemplate: {
                     editable: false,
@@ -109,51 +230,90 @@ export default {
                 ],
                 colModel: [
                     {
+                        title: "管理No",
+                        dataIndx: "チケット管理番号", dataType: "string",align:"right",
+                        width: 90, minWidth: 90, maxWidth: 90,
+                    },
+                    {
                         title: "得意先ＣＤ",
                         dataIndx: "得意先ＣＤ", dataType: "string",align:"right",
-                        width: 90, minWidth: 90, maxWidth: 90,
+                        hidden:true,
                     },
                     {
                         title: "得意先名",
                         dataIndx: "得意先名", dataType: "string",
-                        width: 230, minWidth: 230, maxWidth: 230,
+                        width: 280, minWidth: 280, maxWidth: 280,
                         tooltip: true,
                     },
                     {
-                        title: "得意先名カナ",
-                        dataIndx: "得意先名カナ", dataType: "string",
-                        width: 180, minWidth: 180, maxWidth: 180,
-                    },
-                    {
-                        title: "郵便番号",
-                        dataIndx: "郵便番号", dataType: "string",
-                        width: 90, minWidth: 90, maxWidth: 90,
-                    },
-                    {
-                        title: "住所１",
-                        dataIndx: "住所１", dataType: "string",
-                        width: 230, minWidth: 230, maxWidth: 230,
-                        tooltip: true,
-                    },
-                    {
-                        title: "電話番号",
-                        dataIndx: "電話番号1", dataType: "string",
+                        title: "発効日付",
+                        dataIndx: "F発行日", dataType: "string",
                         width: 120, minWidth: 120, maxWidth: 120,
                     },
                     {
-                        title: "売現区分",
-                        dataIndx: "売掛現金区分", dataType: "string",
-                        width: 80, minWidth: 80, maxWidth: 80,
+                        title: "有効期限",
+                        dataIndx: "F有効期限", dataType: "string",
+                        width: 120, minWidth: 120, maxWidth: 120,
                     },
                     {
-                        title: "締区分",
-                        dataIndx: "締区分", dataType: "string",
-                        width: 80, minWidth: 80, maxWidth: 80,
+                        title: "印刷日時",
+                        dataIndx: "F印刷日時", dataType: "string",
+                        width: 120, minWidth: 120, maxWidth: 120,
                     },
                     {
-                        title: "締日１",
-                        dataIndx: "締日１", dataType: "string",
-                        width: 80, minWidth: 80, maxWidth: 80,
+                        title: "単価",
+                        dataIndx: "単価", dataType: "integer", format: "#,###",　
+                        width: 100, minWidth: 100, maxWidth: 100,
+                        summary: {
+                            type: "TotalInt",
+                        },
+                    },
+                    {
+                        title: "券数",
+                        dataIndx: "チケット内数", dataType: "integer", format: "#,###",　
+                        width: 100, minWidth: 100, maxWidth: 100,
+                        summary: {
+                            type: "TotalInt",
+                        },
+                    },
+                    {
+                        title: "サービス",
+                        dataIndx: "SV内数", dataType: "integer", format: "#,###",　
+                        width: 100, minWidth: 100, maxWidth: 100,
+                        summary: {
+                            type: "TotalInt",
+                        },
+                    },
+                    {
+                        title: "責任者",
+                        dataIndx: "担当者名", dataType: "string",
+                        width: 150, minWidth: 150, maxWidth: 150,
+                    },
+                    {
+                        title: "捨",
+                        dataIndx: "廃棄対象",
+                        type: "checkbox",
+                        cbId: "IncludesSummary",
+                        width: 50, minWidth: 50, maxWidth: 50,
+                        align: "center",
+                        editable: true,
+                        editor: false,
+                        hiddenOnExport: true,
+                        render: ui => {
+                            if (ui.rowData.summaryRow) {
+                                return "";
+                            }
+                        },
+                    },
+                    {
+                        dataIndx: "IncludesSummary",
+                        dataType: "bool",
+                        align: "center",
+                        editable: true,
+                        cb: {
+                            header: true,
+                        },
+                        hidden: false,
                     },
                 ],
             },
@@ -162,9 +322,18 @@ export default {
     methods: {
         createdFunc: function(vue) {
             vue.footerButtons.push(
+                { visible: "true", value: "クリア", id: "DAI06010Grid1_Clear", disabled: false, shortcut: "F2",
+                    onClick: function () {
+                    }
+                },
                 { visible: "true", value: "検索", id: "DAI06010Grid1_Search", disabled: false, shortcut: "F5",
                     onClick: function () {
                         vue.conditionChanged();
+                    }
+                },
+                { visible: "true", value: "登録", id: "DAI06010Grid1_Save", disabled: false, shortcut: "F6",
+                    onClick: function () {
+                        vue.save();
                     }
                 },
                 { visible: "true", value: "CSV", id: "DAI06010Grid1_Csv", disabled: false, shortcut: "F10",
@@ -175,12 +344,19 @@ export default {
             );
         },
         mountedFunc: function(vue) {
-            //日付の初期値 -> 当日
-            //TODO:
-            //vue.viewModel.CourseCd=101;
             var vue = this;
-            //条件変更ハンドラ
-            vue.conditionChanged();
+            //日付の初期値 -> 当日
+            vue.viewModel.DateStart = moment().startOf('month').format("YYYY年MM月DD日");
+            vue.viewModel.DateEnd = moment().endOf('month').format("YYYY年MM月DD日");
+
+            //TODO:開発用に条件を書き込む
+            vue.viewModel.CustomerCd="25997";
+            vue.viewModel.DateStart = moment("2019/04/01");
+            vue.viewModel.DateEnd = moment("2019/04/30");
+        },
+        onChanged: function(code, entities) {
+            var vue = this;
+            //ダミーハンドラ
         },
         onBushoChanged: function(code, entities) {
             var vue = this;
@@ -193,11 +369,57 @@ export default {
             if (!grid || !vue.getLoginInfo().isLogOn) return;
             if (!vue.viewModel.BushoCd) return;
             var params = $.extend(true, {}, vue.viewModel);
+            //パラメータの書式を調整
+            params.DateStart=moment(params.DateStart,"YYYY年MM月DD日").format("YYYY/MM/DD");
+            params.DateEnd=moment(params.DateEnd,"YYYY年MM月DD日").format("YYYY/MM/DD");
+
             grid.searchData(params, false, null, callback);
         },
         onAfterSearchFunc: function (vue, grid, res) {
             var vue = this;
             return res;
+        },
+        CustomerAutoCompleteFunc: function(input, dataList, comp) {
+            var vue = this;
+
+            if (!dataList.length) return [];
+
+            var keywords = input.split(/[, 、　]/).map(v => _.trim(v)).filter(v => !!v);
+            var keyAND = keywords.filter(k => k.match(/^[\+＋]/)).map(k => k.replace(/^[\+＋]/, ""));
+            var keyOR = keywords.filter(k => !k.match(/^[\+＋]/));
+
+            var wholeColumns = ["CdNm", "得意先名略称", "得意先名カナ", "備考１", "備考２", "備考３"];
+
+            if ((input == comp.selectValue && comp.isUnique) || comp.isError) {
+                keyAND = keyOR = [];
+            }
+
+            var list = dataList
+                .filter(v => (!!vue.viewModel.BushoCd && !!vue.viewModel.CourseCd) ? (v.部署CD == vue.viewModel.BushoCd && v.コースＣＤ == vue.viewModel.CourseCd) : true)
+                .map(v => { v.whole = _(v).pickBy((v, k) => wholeColumns.includes(k)).values().join(""); return v; })
+                .filter(v => {
+                    return keyOR.length == 0
+                        || _.some(keyOR, k => v.Cd.startsWith(k))
+                        || _.some(keyOR, k => k.match(/^[0-9\-]{6,}/) != null && !!v.電話番号１ ? v.電話番号１.replace(/-/g, "").includes(k.replace(/-/g, "")) : false)
+                        || _.some(keyOR, k => v.whole.includes(k))
+                })
+                .filter(v => {
+                    return keyAND.length == 0 || _.every(keyAND, k => (v.whole + (v.電話番号１ || "")).includes(k));
+                })
+                .map(v => {
+                    var ret = v;
+                    ret.label = v.Cd + " : " + "【" + v.部署名 + "】" + v.CdNm;
+                    ret.value = v.Cd;
+                    ret.text = v.CdNm;
+                    return ret;
+                })
+                ;
+
+            return list;
+        },
+        save()
+        {
+
         },
     }
 }
