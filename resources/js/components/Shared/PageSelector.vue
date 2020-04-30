e6099a805e94d5927daf52f8d8bc883b5e5736ae
KenjiMatsubara <matsubara@n-e-w.jp>
2020-04-30 21:07:58 +0900

    6050

--- resources/js/components/Shared/PageSelector.vue
+++ resources/js/components/Shared/PageSelector.vue
@@ -140,6 +140,7 @@ import DAI06010 from "@vcp/DAI06010.vue";
 import DAI06020 from "@vcp/DAI06020.vue";
 import DAI06030 from "@vcp/DAI06030.vue";
 import DAI06040 from "@vcp/DAI06040.vue";
+import DAI06050 from "@vcp/DAI06050.vue";
 
 import DAI07010 from "@vcp/DAI07010.vue";
 import DAI07020 from "@vcp/DAI07020.vue";
@@ -274,6 +275,7 @@ export default {
         DAI06020,
         DAI06030,
         DAI06040,
+        DAI06050,
         DAI07010,
         DAI07020,
         DAI07030,
