119b0d0b95ec94cf685f03211fb0213bba03e796
hitoshi-katsumata <katsumata@n-e-w.jp>
2020-05-07 19:35:19 +0900

    06020チケット印刷処理を作成

--- resources/js/components/Pages/DAI06020.vue
+++ resources/js/components/Pages/DAI06020.vue
@@ -210,9 +210,13 @@ export default {
             viewModel: {
                 BushoCd: null,
                 CustomerCd: null,
+                CustomerName: null,
+                ProductCd: null,
+                ProductName: null,
                 InsatsuMaisu:"5",
                 InsatsuTantoCd:null,
                 LastTicketNo:null,
+                NowTicketNo:null,
                 IssueDate: null,
                 IsPrintIssueDate:"1",
                 ValidPeriod:"12",
@@ -288,6 +292,11 @@ export default {
                         format: "#,###",
                         width: 100, minWidth: 100, maxWidth: 100,
                     },
+                    {
+                        title: "得意先名",
+                        dataIndx: "得意先名", dataType: "string",align:"right",
+                        width: 90, minWidth: 90, maxWidth: 90,
+                    },
                 ],
             },
         });
@@ -300,9 +309,10 @@ export default {
                         vue.conditionChanged();
                     }
                 },
-                { visible: "true", value: "実行", id: "DAI06020Grid1_Save", disabled: false, shortcut: "F6",
+                { visible: "true", value: "実行", id: "DAI06020Grid1_Save", disabled: true, shortcut: "F6",
                     onClick: function () {
-                        vue.Save();
+                        vue.save();
+                        vue.print();
                     }
                 }
             );
@@ -319,10 +329,7 @@ export default {
             this.UpdateExpireDate();
         },
         onValidPeriodChanged: function(code, entities) {
-            console.log("onValidPeriodChanged!");
             var vue = this;
-            window.respc=_.cloneDeep(vue.viewModel);//TODO:
-            //console.log("ValidPeriod="+vue.viewModel.ValidPeriod);
             this.UpdateExpireDate();
         },
         onCustomerChanged: function(code, entities) {
@@ -341,9 +348,14 @@ export default {
         },
         onAfterSearchFunc: function (vue, grid, res) {
             var vue = this;
-            console.log("onAfterSearchFunc");//TODO:
-            window.resres=_.cloneDeep(res);//TODO:
+            if(res.length==0)
+            {
+                vue.footerButtons.find(v => v.id == "DAI06020Grid1_Save").disabled = true;
+                vue.viewModel.LastTicketNo=null;
+                return res;
+            }
             vue.viewModel.LastTicketNo=res[0].チケット管理番号 == null ? 0 : res[0].チケット管理番号;
+            vue.footerButtons.find(v => v.id == "DAI06020Grid1_Save").disabled = (!vue.viewModel.LastTicketNo);
             return res;
         },
         CustomerAutoCompleteFunc: function(input, dataList, comp) {
@@ -390,7 +402,7 @@ export default {
             var vue = this;
             vue.viewModel.ExpireDate = moment(vue.viewModel.IssueDate,"YYYY年MM月DD日").add(vue.viewModel.ValidPeriod,'month').add(-1,'day').format("YYYY年MM月DD日");
         },
-        Save:function()
+        save:function()
         {
             console.log("save");//TODO:
             var vue=this;
@@ -412,25 +424,30 @@ export default {
             var SaveList=[];
             _.forEach(grid.pdata,r=>{
                 var SaveItem={};
+                SaveItem.得意先名=r.得意先名;
                 SaveItem.商品ＣＤ=r.商品ＣＤ;
+                SaveItem.商品名=r.商品名;
                 SaveItem.単価=r.単価;
                 SaveList.push(SaveItem);
             });
 
             //登録実行
+            vue.viewModel.CustomerName=SaveList[0].得意先名;
+            vue.viewModel.ProductCd=SaveList[0].商品ＣＤ;
+            vue.viewModel.ProductName=SaveList[0].商品名;
+            vue.viewModel.Price=SaveList[0].単価;
+            vue.viewModel.NowTicketNo=(vue.viewModel.LastTicketNo*1)+1;//チケット印刷をするため、チケット番号を退避
             grid.saveData(
                 {
                     uri: "/DAI06020/Save",
                     params: {
-                        InsatsuMaisu:vue.viewModel.InsatsuMaisu,
                         CustomerCd:vue.viewModel.CustomerCd,
-                        TicketNo:(vue.viewModel.LastTicketNo*1)+1,
+                        TicketNo:vue.viewModel.NowTicketNo*1,
                         IssueDate:moment(vue.viewModel.IssueDate, "YYYY年MM月DD日").format("YYYYMMDD"),
                         ExpireDate:moment(vue.viewModel.ExpireDate, "YYYY年MM月DD日").format("YYYYMMDD"),
                         TicketSu:0,
                         SvTicketSu:0.0,
                         InsatsuTantoCd:vue.viewModel.InsatsuTantoCd,
-                        ShuseiTantoCd:vue.getLoginInfo()["uid"],
                         SaveList: SaveList,
                     },
                     optional: vue.searchParams,
@@ -444,6 +461,183 @@ export default {
                     },
                 }
             );
+        },
+        print:function()
+        {
+            var vue = this;
+            var grid = vue.DAI06020Grid1;
+
+            //印刷用HTML全体適用CSS
+            var globalStyles = `
+                body {
+                    -webkit-print-color-adjust: exact;
+                }
+                table {
+                    table-layout: fixed;
+                    margin-left: 0px;
+                    margin-right: 0px;
+                    width: 100%;
+                    border-spacing: unset;
+                    border: solid 0px black;
+                }
+                th, td {
+                    font-family: "MS UI Gothic";
+                    font-size: 8pt;
+                    font-weight: normal;
+                    margin: 0px;
+                    padding-left: 3px;
+                    padding-right: 3px;
+                    height:80px;
+                }
+                div.ticket-content{
+                    display:inline-block;
+                    vertical-align:top;
+                }
+                .pagebreak {
+                break-before: page;
+                }
+            `;
+
+            var ticketFunc=(seq,isService)=>{
+                var str_TicketNo = ( '000000' + vue.viewModel.NowTicketNo ).slice( -6 );
+                var str_seq = ( '00' + seq ).slice( -2 );
+                var str_service = isService ? "(サービス)":"";
+                var str_IssueDate= vue.viewModel.IsPrintIssueDate=='1' ? "発行:"+ moment(vue.viewModel.IssueDate, "YYYY年MM月DD日").format("YYYY/MM/DD") : "";
+                var str_ExpireDate=vue.viewModel.IsPrintExpireDate=='1' ? "有効期限:"+moment(vue.viewModel.ExpireDate, "YYYY年MM月DD日").format("YYYY/MM/DD") : "";
+                var str_icon_filename=vue.viewModel.BushoCd==701?"701.jpg":"101.jpg";
+                return `
+                    <div>
+                        <div style="width:98%;">
+                            <div class="ticket-content" style="padding:1px 0px 0px 1px;">
+                                <img src="./images/TicketIcon/${str_icon_filename}" style="width:15px;">
+                            </div>
+                            <div class="ticket-content" style="width:89%;padding-top:5px;margin-bottom:5px;">
+                                <div class="ticket-content">${vue.viewModel.CustomerName}</div>
+                                <div class="ticket-content" style="text-align:right; float:right">様<br/>(${vue.viewModel.CustomerCd})</div>
+                                <div style="clear:both"></div>
+                            </div>
+                            <div style="width:98%;text-align:center;margin-bottom:5px;">
+                                ${vue.viewModel.ProductName}${str_service}&nbsp;&nbsp;&yen;${vue.viewModel.Price}
+                            </div>
+                            <div class="ticket-content">${str_IssueDate}</div>
+                            <div class="ticket-content" style="text-align:right; float:right">${str_ExpireDate}</div>
+                            <div style="clear:both"></div>
+                            <div class="ticket-content">株式会社ダイヤス食品</div>
+                            <div class="ticket-content" style="text-align:right; float:right">No.${str_TicketNo}${str_seq}</div>
+                            <div style="clear:both"></div>
+                        </div>
+                    </div>
+                `;
+            };
+
+            var ticket_caution=`
+                    <div style="text-decoration:underline;font-size:10pt;line-height:18px;">
+                    ※有効期限以降の使用はできません。<br/>
+                    ※記載されているお客様以外の方は<br/>
+                    使用できません。<br/>
+                    </div>
+                `;
+            var ticket_blank=`
+                    <div style="text-align:center;font-size:18pt;">
+                    ＊＊＊＊＊
+                    </div>
+                `;
+
+            var maisu=30;
+            var service_maisu=2;
+
+            var layout_content=[];
+
+            //指定枚数のチケットを作成する
+            var i=0;
+            [...Array(maisu)].map(r=>{
+                    layout_content[i]={val:i,data:ticketFunc(i+1,false)};
+                    i++;
+                });
+
+            //指定枚数のサービスチケットを作成する
+            [...Array(service_maisu)].map(r=>{
+                    layout_content[i]={val:i,data:ticketFunc(i+1,true)};
+                    i++;
+                });
+
+            //コーションを作成する
+            layout_content[i]={val:i,data:ticket_caution};
+            i++;
+
+            //ブランクを作成する
+            var blank_su=27 - ((maisu+service_maisu+1) % 27);
+            [...Array(blank_su)].map(r=>{
+                    layout_content[i]={val:i,data:ticket_blank};
+                    i++;
+                });
+
+            //チケットをページごと(1ページ当り27チケット)に分割し、用紙の左上から縦並びに並べ替える
+            var layout_content = _.chunk(layout_content,27).map(r=> _.sortBy(r,v=>v.val%9));
+
+            //ページの作成
+            var page=0;
+            var layout_page_html = layout_content.map(lc=>{
+                var page_break= page!=0?`<div class="pagebreak"></div>`:``;
+                var layout_ticket_html=``;
+                var j=0;
+                [...Array(9)].map(r=>{
+                        layout_ticket_html += `
+                            <tr>
+                                <td>
+                                    ${lc[j].data}
+                                </td>
+                                <td>
+                                    ${lc[j+1].data}
+                                </td>
+                                <td>
+                                    ${lc[j+2].data}
+                                </td>
+                            </tr>
+                        `;
+                        j+=3;
+                    });
+                page++;
+                return `
+                    ${page_break}
+                    <table style="width:100%;">
+                        ${layout_ticket_html}
+                    </table>
+                `;
+            });
+
+            //全ページの結合
+            var layout=_.join(layout_page_html,'');
+
+            //出力実行
+            var target ={ contents: layout };
+            var printable = $("<html>")
+                .append($("<head>").append($("<style>").text(globalStyles)))
+                .append(
+                    $("<body>")
+                        .append(
+                            grid.generateHtmlFromJson(
+                                target,
+                                ``,
+                                null,
+                                1,
+                                false,
+                                true,
+                            )
+                        )
+                )
+                .prop("outerHTML")
+                ;
+
+            //TODO:カラー印刷未対応
+            var printOptions = {
+                type: "raw-html",
+                style: "@media print { @page { size: A4 portrait; } }",
+                printable: printable,
+            };
+            printJS(printOptions);
+            //TODO: 印刷用HTMLの確認はデバッグコンソールで以下を実行
+            //$("#printJS").conten
         },
     }
 }
