cbe0e4a0f72a69e25862f7b4d0ed21ff824018a5
hitoshi-katsumata <katsumata@n-e-w.jp>
2020-07-02 17:40:37 +0900

    モバイルデータ送信の非同期送信対応

--- app/Console/Commands/MobileDataReceive.php
+++ app/Console/Commands/MobileDataReceive.php
@@ -8,30 +8,12 @@ use App\Libs\PWADataReceive;
 //AWS(PWA)から社内DBへ取込
 class MobileDataReceive extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'batch:MobileDataReceive';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
-
     /**
      * Execute the console command.
      *
