7c7a6f0bebce9efe79ee7c02e22f29557e4e7bb1
hitoshi-katsumata <katsumata@n-e-w.jp>
2020-07-13 23:56:52 +0900

    バッチにPWAへのデータ送信処理を追加

--- app/Console/Commands/MobileDataReceive.php
+++ app/Console/Commands/MobileDataReceive.php
@@ -4,12 +4,13 @@ namespace App\Console\Commands;
 use Exception;
 use Illuminate\Console\Command;
 use App\Libs\PWADataReceive;
+use App\Libs\PWADataSend;
 
-//AWS(PWA)から社内DBへ取込
+//AWS(PWA)から社内DBへ取込/送信
 class MobileDataReceive extends Command
 {
     protected $signature = 'batch:MobileDataReceive';
-    protected $description = 'AWS(PWA)から社内DBへ更新データを取込';
+    protected $description = 'AWS(PWA)から社内DBへ更新データを取込/送信';
     public function __construct()
     {
         parent::__construct();
@@ -22,8 +23,11 @@ class MobileDataReceive extends Command
     public function handle()
     {
         try {
+            $ds = new PWADataSend();
+            $ds->Send();
             $dr = new PWADataReceive();
             $dr->Receive();
+
         }
         catch (Exception $exception) {
             echo $exception;
