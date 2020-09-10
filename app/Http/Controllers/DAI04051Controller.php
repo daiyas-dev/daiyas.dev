bc672a182257833338d98e399e1d04f2b71d9d6f
hitoshi-katsumata <katsumata@n-e-w.jp>
2020-09-10 15:35:51 +0900

    単価マスタ更新後のモバイルデータ連携を非同期に変更

--- app/Http/Controllers/DAI04051Controller.php
+++ app/Http/Controllers/DAI04051Controller.php
@@ -142,7 +142,7 @@ class DAI04051Controller extends Controller
             //モバイルsv更新
             $Message = $params['Message'];
             $ds = new DataSendWrapper();
-            $ds->Delete('得意先単価マスタ新', $params, true, null, $params['得意先ＣＤ'], null, $Message);
+            $ds->Delete('得意先単価マスタ新', $params, false, null, $params['得意先ＣＤ'], null, $Message);
         } catch (Exception $exception) {
             DB::rollBack();
             throw $exception;
