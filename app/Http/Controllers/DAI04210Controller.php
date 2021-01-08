151bdea24a4d267d6cf790fcc62eca68b5ef07e8
hitoshi-katsumata <katsumata@n-e-w.jp>
2021-01-08 15:36:25 +0900

    レコード番号387 非顧客電話番号の得意先取得SQLを改修しました。

--- app/Http/Controllers/DAI04210Controller.php
+++ app/Http/Controllers/DAI04210Controller.php
@@ -22,14 +22,26 @@ class DAI04210Controller extends Controller
         }
 
         $sql = "
-                    SELECT
-                        TEL.電話番号
-                        ,ISNULL(CM.得意先名略称,'得意先マスタに登録なし')AS 得意先名
-                    FROM
-                        非顧客電話番号マスタ TEL
-                        LEFT JOIN 得意先マスタ CM ON CM.電話番号１=TEL.電話番号
-                    WHERE 0=0
-                    　    $WhereTelNo
+                WITH TEL2CUST AS(
+                    SELECT Q.* FROM(
+                        SELECT
+                        TC.Tel_TelNo
+                        ,TC.Tel_CustNo
+                        ,RANK() OVER(PARTITION BY TC.Tel_TelNo ORDER BY TC.Tel_TelNo,TC.Tel_UpdDate DESC)AS RNK
+                        FROM C_TelToCust TC
+                    )Q
+                    WHERE Q.RNK=1
+                )
+                SELECT
+                    TEL.電話番号
+                    ,ISNULL(CM1.得意先名略称,ISNULL(CM2.得意先名略称,'得意先マスタに登録なし'))AS 得意先名
+                FROM
+                    非顧客電話番号マスタ TEL
+                    LEFT JOIN 得意先マスタ CM1 ON CM1.電話番号１=TEL.電話番号
+                    LEFT JOIN TEL2CUST T2C ON T2C.Tel_TelNo=TEL.電話番号
+                    LEFT JOIN 得意先マスタ CM2 ON CM2.得意先ＣＤ=T2C.Tel_CustNo
+                WHERE 0=0
+                    $WhereTelNo
                 ";
 
         $DataList = DB::select($sql);
